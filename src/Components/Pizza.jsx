631eb1745c840f07c79a1bf38437d95177b5c3d9
Ryavnn <ryanisnjoroge@gmail.com>
2024-02-22 10:34:47 +0300

    added images but importation doesnt work

--- src/Components/Pizza.jsx
+++ src/Components/Pizza.jsx
@@ -1,27 +1,12 @@
+import burger from "../public/assets/Burger.jpg"
 function Pizza(){
     return(
         <section className="variety">
+            <h1>PizzaGate</h1>
             <h1>Pizzas we offer at our Restaurant</h1>
             <div className="pizzas">
-                <div className="container-item">
-                    <img src="../assets/pizzaa.jpg" alt=" " />
-                    <h2>Pepperoni Pizza</h2>
-                    <p>A classic favorite feauturing savory</p>
-                </div>
-                <div className="container-item">
-                    <img src="../assets/pizzaa.jpg" alt=" " />
-                    <h2>Vegeterian Delight</h2>
-                    <p>A classic favorite feauturing savory</p>
-                </div>
-                <div className="container-item">
-                    <img src="../assets/pizzaa.jpg" alt=" " />
-                    <h2>Margherita Pizza</h2>
-                    <p>A classic favorite feauturing savory</p>
-                </div>
-                <div className="container-item">
-                    <img src="../assets/pizzaa.jpg" alt=" " />
-                    <h2>Meat Lovers Feast</h2>
-                    <p>A classic favorite feauturing savory</p>
+                <div className="container">
+                    <img src={burger} alt="" />
                 </div>
             </div>
         </section>
